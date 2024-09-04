c03e84393c93eea468ea8f6e09215cd42979211d
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-05 04:59:22 +0530

    add while in view to img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ export class Hero extends Component {
               <motion.h1 variants={container(0)} initial="hidden" whileInView="visible" className="pb-12 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl ">
                 Prakash Kumar
               </motion.h1>
-              <motion.span variants={container(0.5)} initial="hidden" whileInView="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              <motion.span variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                 Full Stack Developer
               </motion.span>
               <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">
@@ -28,7 +28,7 @@ export class Hero extends Component {
           </div>
           <div className="w-full lg:w-1/2 lg:p-8">
             <div className="flex justify-center">
-              <motion.img initial={{x:100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:1,delay:1.2}} src="https://iili.io/dkVxWzB.gif" alt="" className="w-[36rem]" />
+              <motion.img initial={{x:100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1,delay:1.2}} src="https://iili.io/dkVxWzB.gif" alt="" className="w-[36rem]" />
             </div>
           </div>
         </div>
