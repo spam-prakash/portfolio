3362685ffa964cc4035d6dedafc1a1d94ce1a1d0
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-05 04:51:47 +0530

    fix phone call link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export class Contact extends Component {
         <motion.h2 initial={{opacity:0,y:-50}} whileInView={{opacity:1, y:0}} transition={{delay:0,duration:0.5}} className='my-10 text-center text-4xl'>Contact Me</motion.h2>
         <div className="text-center tracking-tighter">
             <motion.p initial={{opacity:0,x:-100}} whileInView={{opacity:1,x:0}} transition={{duration:1}} className="my-4">{CONTACT.address}</motion.p>
-            <motion.a initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={{duration:1}} className="my-4" href="tel:950-853-2566">{CONTACT.phoneNo}</motion.a>
+            <motion.p initial={{opacity:0,x:100}} whileInView={{opacity:1,x:0}} transition={{duration:1}} className="my-4"><a href="tel:950-853-2566">+91 9508532566</a></motion.p>
             <a href="mailto:prakashkumar01031975@gmail.com" className="my-4">{CONTACT.email}</a>
         </div>
       </div>
